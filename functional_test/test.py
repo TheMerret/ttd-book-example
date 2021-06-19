74ad1a183750589a0d54f5b24a7948db036ad0a0
Kharitonov Maxim <kharitonmax@gmail.com>
2021-06-19 19:38:29 +0300

    новый URL, представление и шаблон, который отображает списки

--- functional_test/test.py
+++ functional_test/test.py
@@ -96,7 +96,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Фрэнсис посещает домашнюю страницу. Нет никаких признаков списка Эдит
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertNotIn('Сделать мушку', page_text)
 
         # Фрэнсис начинает новый список, вводя новый элемент. Он менее
