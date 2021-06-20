cdfc5c32bb170198b7709bcbbfd9b5d5cb1cc755
Kharitonov Maxim <kharitonmax@gmail.com>
2021-06-20 15:11:51 +0300

    Добавлен URL для создания нового списка посредством POST, рефакторизация модульных тестов

--- lists/tests.py
+++ lists/tests.py
@@ -10,24 +10,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_POST_request(self):
-        """тест: можно сохранить post-запрос"""
-        self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        """тест: переадресует после post-запроса"""
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/one-of-a-kind-list-in-the-world')
-
-    def test_only_saves_items_when_necessary(self):
-        """тест: сохраняет элементы, только когда нужно"""
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
     """тест модели элемента списка"""
@@ -56,7 +38,7 @@ class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         """тест: используется шаблон списка"""
-        response = self.client.get('/lists/one-of-a-kind-list-in-the-world')
+        response = self.client.get('/lists/one-of-a-kind-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
@@ -64,7 +46,23 @@ class ListViewTest(TestCase):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/lists/one-of-a-kind-list-in-the-world')
+        response = self.client.get('/lists/one-of-a-kind-list-in-the-world/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+    """тест нового списка"""
+
+    def test_can_save_POST_request(self):
+        """тест: можно сохранить post-запрос"""
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        """тест: переадресует после post-запроса"""
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/one-of-a-kind-list-in-the-world/')
