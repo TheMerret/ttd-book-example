f66056646482f958a74c3d78e59fd5a6e85c0d51
Kharitonov Maxim <kharitonmax@gmail.com>
2021-08-13 16:57:02 +0300

    Форма теперь используется в представлении. Рабочее состояние

--- lists/forms.py
+++ lists/forms.py
@@ -32,6 +32,7 @@ class ExistingListItemForm(ItemForm):
 
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.instance.list = for_list
 
     def validate_unique(self):
         """проверка уникальности"""
@@ -40,3 +41,6 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+    def save(self, commit=True):
+        return forms.models.ModelForm.save(self, commit=commit)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,8 +1,8 @@
-from unittest import skip
 from django.test import TestCase
 from django.utils.html import escape
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+                         ExistingListItemForm, ItemForm)
 
 
 class HomePageTest(TestCase):
@@ -43,6 +43,13 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
+
+    def test_displays_item_form(self):
+        """тест: отображается форма для элемента списка"""
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
+        self.assertContains(response, 'name="text"')
 
     def test_passes_correct_list_to_template(self):
         """тест: передается правильный шаблон списка"""
@@ -100,14 +107,13 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_passes_form_to_template(self):
         """тест на недопустимый ввод: форма передается в шаблон"""
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         """тест на недопустимый ввод: на странице показывается ошибка"""
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         """тест: ошибки валидации повторяющегося элемента
            оканчиваются на странице списков"""
@@ -117,7 +123,7 @@ class ListViewTest(TestCase):
             f'/lists/{list1.id}/',
             data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
