1db0b0ab0f6c606fd6794d6110c0b01b2b1c43f6
Kharitonov Maxim <kharitonmax@gmail.com>
2021-08-15 18:08:08 +0300

    Добавлен вход в сессию для аутентифицированных пользователей

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
 from accounts.models import Token
 
@@ -27,4 +27,7 @@ def send_login_email(request):
 
 
 def login(request):
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
     return redirect('/')
