253e9bc230f48558367b6a57712cb4513d5a0443
Kharitonov Maxim <kharitonmax@gmail.com>
2021-08-17 17:12:20 +0300

    URL, представление-заготовка и примерные шаблоны для my_lists

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -166,3 +166,12 @@ class NewListTest(TestCase):
         self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+
+
+class MyListTest(TestCase):
+    """Тест моих списков"""
+
+    def test_my_lists_url_renders_my_lists_template(self):
+        """URL «мои списки» использует шаблон «мои списки»"""
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertTemplateUsed(response, 'my_lists.html')
